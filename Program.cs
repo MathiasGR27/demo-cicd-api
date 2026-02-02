86a678fe9547c71c3b39d4e123ab688c7be4c340
MathiasGR27 <megualpa@espe.edu.ec>
2026-02-02 11:38:09 -0500

    feat: modificacion program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/api/v1/ping", () => Results.Ok(new { ok = true, ts = DateTime.UtcNow }));
+// app.MapGet("/api/v1/ping", () => Results.Ok(new { ok = true, ts = DateTime.UtcNow }));
+app.MapGet("/api/v1/ping", () => Results.Ok(new { message = "¡CI/CD funcionando perfectamente!", ts = DateTime.UtcNow }));
 app.MapGet("/health", () => Results.Ok("OK"));
 app.MapGet("/version", () => Results.Ok(new {
     service = "Demo.CICD.Api",
